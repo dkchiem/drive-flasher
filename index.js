54b5c2091455c9c36cbc88b612fc85e59524e41d
Vu Dang Khoa Chiem <cvdkhoa@gmail.com>
2020-05-16 18:24:20 -0400

    refactor

--- index.js
+++ index.js
@@ -7,101 +7,78 @@ const files = require('./lib/files');
 const { checkUpdate } = require('./lib/update');
 // npm
 const inquirer = require('inquirer');
-const exec = require('child_process').exec;
 const ora = require('ora');
 require('colors');
 
 main();
 
-function main() {
-  drives.getExternalDrives((removableDrives) => {
-    let driveQuestion = [];
+async function main() {
+  const removableDrives = await drives.getExternalDrives();
+  let driveQuestion = [];
 
-    Object.keys(removableDrives).forEach((key, index) => {
-      driveQuestion.push({
-        name: removableDrives[0].name,
-        value: index,
-      });
+  Object.keys(removableDrives).forEach((key, index) => {
+    driveQuestion.push({
+      name: removableDrives[0].name,
+      value: index,
     });
-
-    // Question 1
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'driveIndex',
-          message: 'Which removable drive do you want to use?',
-          choices: driveQuestion,
-        },
-      ])
-      .then(({ driveIndex }) => {
-        // Question 2
-        inquirer
-          .prompt([
-            {
-              type: 'input',
-              name: 'imageFile',
-              message: "What's the path to your image file",
-            },
-          ])
-          .then(({ imageFile }) => {
-            const tempFilesPath = __dirname + '/temp';
-            const dmgPartialPath = tempFilesPath + '/' + files.getFileName(imageFile);
-            const dmgFullPath = dmgPartialPath + '.dmg';
-
-            const spinner = ora(`Erasing "${removableDrives[driveIndex].name}" drive...`).start();
-            spinner.color = 'green';
-
-            // Erase disk
-            shell
-              .execCommand(
-                `diskutil eraseDisk JHFS+ DiskName ${removableDrives[driveIndex].device}`
-              )
-              .then(() => {
-                // Convert temp folder
-                spinner.text = 'Converting image to .dmg file...';
-                shell.execCommand(`mkdir -p ${tempFilesPath}`).then(() => {
-                  // Convert image to .dmg file
-                  shell
-                    .execCommand(`hdiutil convert -format UDRW -o ${dmgPartialPath} ${imageFile}`)
-                    .then(() => {
-                      // Unmount drive
-                      spinner.text = 'Unmounting drive...';
-                      shell
-                        .execCommand(`diskutil unmountDisk ${removableDrives[driveIndex].device}`)
-                        .then(() => {
-                          // Flash drive
-                          spinner.text = 'Flashing drive... (This will take a few minutes)';
-                          shell
-                            .execCommand(
-                              `sudo dd if=${dmgFullPath} of=${removableDrives[driveIndex].raw} bs=1m`
-                            )
-                            .then(() => {
-                              // Clean up temp files created
-                              spinner.text = 'Cleaning created temp files...';
-                              shell.execCommand(`rm ${dmgFullPath}`).then(() => {
-                                spinner.text = 'Ejecting drive...';
-                                // Eject drive
-                                shell
-                                  .execCommand(
-                                    `diskutil eject ${removableDrives[driveIndex].device}`
-                                  )
-                                  .then(() => {
-                                    // DONE!
-                                    spinner.text = 'Done!';
-                                    setTimeout(() => {
-                                      spinner.stop();
-                                      console.log('Thank you for using drive-flasher!'.blue.bold);
-                                      checkUpdate();
-                                    }, 1000);
-                                  });
-                              });
-                            });
-                        });
-                    });
-                });
-              });
-          });
-      });
   });
+
+  const { driveIndex, imageFile, eraseConfirm } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'driveIndex',
+      message: 'Which removable drive do you want to use',
+      choices: driveQuestion,
+    },
+    {
+      type: 'input',
+      name: 'imageFile',
+      message: "What's the path to your image file",
+      validate(path) {
+        if (!path) {
+          return 'Please enter the path to your image file';
+        } else if (!files.checkPath(path)) {
+          return 'Invalid path';
+        }
+        return true;
+      },
+    },
+    {
+      type: 'confirm',
+      name: 'eraseConfirm',
+      message: 'Are you sure you want to flash this drive? This wipe everything on the it.',
+    },
+  ]);
+  if (!eraseConfirm) process.exit(0);
+  const tempFilesPath = __dirname + '/temp';
+  const dmgPartialPath = tempFilesPath + '/' + files.getFileName(imageFile);
+  const dmgFullPath = dmgPartialPath + '.dmg';
+
+  // Erase disk
+  const spinner = ora(`Erasing "${removableDrives[driveIndex].name}" drive...`).start();
+  spinner.color = 'green';
+  await shell.execCommand(
+    `diskutil eraseDisk JHFS+ DiskName ${removableDrives[driveIndex].device}`
+  );
+  // Convert temp folder
+  spinner.text = 'Converting image to .dmg file...';
+  await shell.execCommand(`mkdir -p ${tempFilesPath}`);
+  // Convert image to .dmg file
+  await shell.execCommand(`hdiutil convert -format UDRW -o ${dmgPartialPath} ${imageFile}`);
+  // Unmount drive
+  spinner.text = 'Unmounting drive...';
+  await shell.execCommand(`diskutil unmountDisk ${removableDrives[driveIndex].device}`);
+  // Flash drive
+  spinner.text = 'Flashing drive... (This will take a few minutes)';
+  await shell.execCommand(`sudo dd if=${dmgFullPath} of=${removableDrives[driveIndex].raw} bs=1m`);
+  // Clean up temp files created
+  spinner.text = 'Cleaning created temp files...';
+  await shell.execCommand(`rm ${dmgFullPath}`);
+  spinner.text = 'Ejecting drive...';
+  // Eject drive
+  await shell.execCommand(`diskutil eject ${removableDrives[driveIndex].device}`);
+  // DONE!
+  spinner.stop();
+  console.log('Thank you for using drive-flasher!'.blue.bold);
+  checkUpdate();
 }
